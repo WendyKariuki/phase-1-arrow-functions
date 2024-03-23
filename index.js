f066d3e9d03d8858d52fc11f49d867858a934a41
WendyKariuki <phaminuswendy@gmail.com>
2024-03-23 23:35:46 +0300

    Arrow function

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function(a, b) {
+    return a / b;
+};
+console.log(divide(2000, 100)); // Output: 5
+
+const square = (num) => {
+    return num * num;
+};
+
+const add = (num1,  num2) => {
+    return num1 + num2;
+};
